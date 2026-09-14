ec68f58d2f9e6e0d0ece34ceeb87f6c5f67280d1
Fellipe Bastos <fellipe.resende@hotmail.com>
2026-09-14 00:29:27 -0300

    chore: criando arquitetura e implementando endpoint da api de health e predictec apenas para testar, sem funcionalidade no momento

---
+++ src/medical_triage/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from medical_triage.api.routes import router
+import uvicorn
+
+app = FastAPI(
+    title="Medical Triage API",
+    version="0.1.0",
+)
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
